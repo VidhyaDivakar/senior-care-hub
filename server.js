1878309b402156c7d39367d306d17e606e2cccf0
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-06-02 11:02:36 -0400

    Phase 2 - Worked on the server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const authRoutes = require("./routes/authRoutes");
+const skillRoutes = require("./routes/skillRoutes");
+const learningRequestRoutes = require("./routes/learningRequestRoutes");
+const communityPostRoutes = require("./routes/communityPostRoutes");
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/skills", skillRoutes);
+app.use("/api/learning-requests", learningRequestRoutes);
+app.use("/api/posts", communityPostRoutes);
+
+// Health Check Route
+app.get("/", (req, res) => {
+    res.json({
+        message: "Senior Community Hub API Running"
+    });
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
