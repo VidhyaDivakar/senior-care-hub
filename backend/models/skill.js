8ca76953c2060671fa7ef19eed544b49fea3f40b
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-06-02 00:27:43 -0400

    Phase 2 - Data Modelling and changed schema file names, modelled skills

---
+++ backend/models/skill.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const skillSchema = new mongoose.Schema({
+
+    title: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+
+    proficiencyLevel: {
+        type: String,
+        enum: ["Beginner", "Intermediate", "Advanced"],
+        default: "Beginner"
+    },
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+
+}, {
+    timestamps: true
+});
+
+const Skill = mongoose.model("Skill", skillSchema);
+
+module.exports = Skill;
